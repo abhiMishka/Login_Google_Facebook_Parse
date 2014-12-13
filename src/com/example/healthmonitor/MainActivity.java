8ec8871203be82650b252ebf20def589aa0c38e2
Abhishek <abhishek11006@iiitd.ac.in>
2014-12-13 04:04:14 -0800

    uploading project
    login options

---
+++ src/com/example/healthmonitor/MainActivity.java
@@ -0,0 +1,588 @@
+package com.example.healthmonitor;
+ 
+import java.io.InputStream;
+import java.util.Locale;
+
+import parseLogin.ConnectionDetector;
+import parseLogin.ForgetParsePassword;
+import parseLogin.Login;
+import parseLogin.SignUp;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.android.AsyncFacebookRunner;
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.DialogListener;
+import com.facebook.android.FacebookError;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+
+ 
+public class MainActivity extends Activity implements OnClickListener,
+        ConnectionCallbacks, OnConnectionFailedListener {
+ 
+    private static final int RC_SIGN_IN = 0;
+    // Logcat tag
+    private static final String TAG = "MainActivity";
+ 
+    // Profile pic image size in pixels
+    private static final int PROFILE_PIC_SIZE = 400;
+ 
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
+ 
+    /**
+     * A flag indicating that a PendingIntent is in progress and prevents us
+     * from starting further intents.
+     */
+    private boolean mIntentInProgress;
+ 
+    private boolean mSignInClicked;
+ 
+    private ConnectionResult mConnectionResult;
+ 
+    private SignInButton btnSignIn;
+    private Button btnSignOut, btnRevokeAccess,btnFbLogin;
+    private ImageView imgProfilePic;
+    private TextView txtName, txtEmail;
+    private LinearLayout llProfileLayout;
+    
+    // Your Facebook APP ID
+    private static String APP_ID = "1563906497157477"; // Replace your App ID here
+ 
+    // Instance of Facebook Class
+    private Facebook facebook;
+    private AsyncFacebookRunner mAsyncRunner;
+    String FILENAME = "AndroidSSO_data";
+    private SharedPreferences mPrefs;
+    Button btn_LoginIn = null;
+	Button btn_SignUp = null;
+	Button btn_ForgetPass = null;
+	private EditText mUserNameEditText;
+	private EditText mPasswordEditText;
+
+	// flag for Internet connection status
+	Boolean isInternetPresent = false;
+	// Connection detector class
+	ConnectionDetector cd;
+
+
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        facebook = new Facebook(APP_ID);
+        mAsyncRunner = new AsyncFacebookRunner(facebook);
+        
+        btnSignIn = (SignInButton) findViewById(R.id.btn_sign_in);
+        btnSignOut = (Button) findViewById(R.id.btn_sign_out);
+        btnRevokeAccess = (Button) findViewById(R.id.btn_revoke_access);
+        imgProfilePic = (ImageView) findViewById(R.id.imgProfilePic);
+        txtName = (TextView) findViewById(R.id.txtName);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+        llProfileLayout = (LinearLayout) findViewById(R.id.llProfile);
+        btnFbLogin = (Button) findViewById(R.id.btn_fblogin);
+        // Button click listeners
+        btnSignIn.setOnClickListener(this);
+        btnSignOut.setOnClickListener(this);
+        btnRevokeAccess.setOnClickListener(this);
+        
+
+		//Initializing Parse SDK
+		onCreateParse();
+		//Calling ParseAnalytics to see Analytics of our app
+		ParseAnalytics.trackAppOpened(getIntent());
+		
+		// creating connection detector class instance
+		cd = new ConnectionDetector(getApplicationContext());
+
+		btn_LoginIn = (Button) findViewById(R.id.btn_login);
+		btn_SignUp = (Button) findViewById(R.id.btn_signup);
+		btn_ForgetPass = (Button) findViewById(R.id.btn_ForgetPass);
+		mUserNameEditText = (EditText) findViewById(R.id.username);
+		mPasswordEditText = (EditText) findViewById(R.id.password);
+
+		
+		btn_LoginIn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				// get Internet status
+				isInternetPresent = cd.isConnectingToInternet();
+				// check for Internet status
+				if (isInternetPresent) {
+					// Internet Connection is Present
+					// make HTTP requests
+					attemptLogin();
+				} else {
+					// Internet connection is not present
+					// Ask user to connect to Internet
+					showAlertDialog(MainActivity.this, "No Internet Connection",
+							"You don't have internet connection.", false);
+				}
+
+			}
+		});
+
+		btn_SignUp.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent in =  new Intent(MainActivity.this,SignUp.class);
+				startActivity(in);
+			}
+		});
+		
+		btn_ForgetPass.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent in =  new Intent(MainActivity.this,ForgetParsePassword.class);
+				startActivity(in);
+			}
+		});
+
+		
+		
+        btnFbLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    loginToFacebook();
+                }
+        });
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN).build();
+    }
+    
+    
+    
+    public void attemptLogin() {
+
+		clearErrors();
+
+		// Store values at the time of the login attempt.
+		String username = mUserNameEditText.getText().toString();
+		String password = mPasswordEditText.getText().toString();
+
+		boolean cancel = false;
+		View focusView = null;
+
+		// Check for a valid password.
+		if (TextUtils.isEmpty(password)) {
+			mPasswordEditText.setError(getString(R.string.error_field_required));
+			focusView = mPasswordEditText;
+			cancel = true;
+		} else if (password.length() < 4) {
+			mPasswordEditText.setError(getString(R.string.error_invalid_password));
+			focusView =mPasswordEditText;
+			cancel = true;
+		}
+
+		// Check for a valid email address.
+		if (TextUtils.isEmpty(username)) {
+			mUserNameEditText.setError(getString(R.string.error_field_required));
+			focusView = mUserNameEditText;
+			cancel = true;
+		}
+
+		if (cancel) {
+			// There was an error; don't attempt login and focus the first
+			// form field with an error.
+			focusView.requestFocus();
+		} else {
+			// perform the user login attempt.
+			login(username.toLowerCase(Locale.getDefault()), password);
+		}
+	}
+    
+    
+    private void login(String lowerCase, String password) {
+		// TODO Auto-generated method stub
+		ParseUser.logInInBackground(lowerCase, password, new LogInCallback() {
+			@Override
+			public void done(ParseUser user, ParseException e) {
+				// TODO Auto-generated method stub
+				if(e == null)
+					loginSuccessful();
+				else
+					loginUnSuccessful();
+			}
+		});
+
+	}
+    
+	protected void loginSuccessful() {
+		// TODO Auto-generated method stub
+		Intent in =  new Intent(MainActivity.this,MainActivity.class);
+		startActivity(in);
+	}
+	protected void loginUnSuccessful() {
+		// TODO Auto-generated method stub
+		Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+		showAlertDialog(MainActivity.this,"Login", "Username or Password is invalid.", false);
+	}
+
+	private void clearErrors(){
+		mUserNameEditText.setError(null);
+		mPasswordEditText.setError(null);
+	}
+
+	@SuppressWarnings("deprecation")
+	public void showAlertDialog(Context context, String title, String message, Boolean status) {
+		AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+
+		// Setting Dialog Title
+		alertDialog.setTitle(title);
+
+		// Setting Dialog Message
+		alertDialog.setMessage(message);
+
+		// Setting alert dialog icon
+		alertDialog.setIcon(R.drawable.fail);
+
+		// Setting OK Button
+		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+			}
+		});
+
+		// Showing Alert Message
+		alertDialog.show();
+	}
+    
+    
+    
+    public void loginToFacebook() {
+        mPrefs = getPreferences(MODE_PRIVATE);
+        String access_token = mPrefs.getString("access_token", null);
+        long expires = mPrefs.getLong("access_expires", 0);
+     
+        if (access_token != null) {
+            facebook.setAccessToken(access_token);
+        }
+     
+        if (expires != 0) {
+            facebook.setAccessExpires(expires);
+        }
+     
+        if (!facebook.isSessionValid()) {
+            facebook.authorize(this,
+                    new String[] { "email", "publish_stream" },
+                    new DialogListener() {
+     
+                        @Override
+                        public void onCancel() {
+                            // Function to handle cancel event
+                        }
+     
+                        @Override
+                        public void onComplete(Bundle values) {
+                            // Function to handle complete event
+                            // Edit Preferences and update facebook acess_token
+                            SharedPreferences.Editor editor = mPrefs.edit();
+                            editor.putString("access_token",
+                                    facebook.getAccessToken());
+                            editor.putLong("access_expires",
+                                    facebook.getAccessExpires());
+                            editor.commit();
+                        }
+     
+                        @Override
+                        public void onError(DialogError error) {
+                            // Function to handle error
+     
+                        }
+     
+                        @Override
+                        public void onFacebookError(FacebookError fberror) {
+                            // Function to handle Facebook errors
+     
+                        }
+     
+                    });
+        }
+    }
+    
+    public void onCreateParse() { 
+		Parse.initialize(this, "XWSbWdI6nNru4rzbZA602bTSfiRBxDRKP767IGDE", "dzOX1ND5UrFaZC9dA9QpV1uTWI1RQIrv2GS57Til"); 
+	}
+ 
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+ 
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+ 
+    /**
+     * Method to resolve any signin errors
+     * */
+    private void resolveSignInError() {
+        if (mConnectionResult.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+                mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+            } catch (SendIntentException e) {
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+ 
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (!result.hasResolution()) {
+            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this,
+                    0).show();
+            return;
+        }
+ 
+        if (!mIntentInProgress) {
+            // Store the ConnectionResult for later usage
+            mConnectionResult = result;
+ 
+            if (mSignInClicked) {
+                // The user has already clicked 'sign-in' so we attempt to
+                // resolve all
+                // errors until the user is signed in, or they cancel.
+                resolveSignInError();
+            }
+        }
+ 
+    }
+ 
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode,
+            Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            if (responseCode != RESULT_OK) {
+                mSignInClicked = false;
+            }
+ 
+            mIntentInProgress = false;
+ 
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+ 
+    @Override
+    public void onConnected(Bundle arg0) {
+        mSignInClicked = false;
+        Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+ 
+        // Get user's information
+        getProfileInformation();
+ 
+        // Update the UI after signin
+        updateUI(true);
+ 
+    }
+ 
+    /**
+     * Updating the UI, showing/hiding buttons and profile layout
+     * */
+    private void updateUI(boolean isSignedIn) {
+        if (isSignedIn) {
+            btnSignIn.setVisibility(View.GONE);
+            btnSignOut.setVisibility(View.VISIBLE);
+            btnRevokeAccess.setVisibility(View.VISIBLE);
+            llProfileLayout.setVisibility(View.VISIBLE);
+        } else {
+            btnSignIn.setVisibility(View.VISIBLE);
+            btnSignOut.setVisibility(View.GONE);
+            btnRevokeAccess.setVisibility(View.GONE);
+            llProfileLayout.setVisibility(View.GONE);
+        }
+    }
+ 
+    /**
+     * Fetching user's information name, email, profile pic
+     * */
+    private void getProfileInformation() {
+        try {
+            if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+                Person currentPerson = Plus.PeopleApi
+                        .getCurrentPerson(mGoogleApiClient);
+                String personName = currentPerson.getDisplayName();
+                String personPhotoUrl = currentPerson.getImage().getUrl();
+                String personGooglePlusProfile = currentPerson.getUrl();
+                String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+ 
+                Log.e(TAG, "Name: " + personName + ", plusProfile: "
+                        + personGooglePlusProfile + ", email: " + email
+                        + ", Image: " + personPhotoUrl);
+ 
+                txtName.setText(personName);
+                txtEmail.setText(email);
+ 
+                // by default the profile url gives 50x50 px image only
+                // we can replace the value with whatever dimension we want by
+                // replacing sz=X
+                personPhotoUrl = personPhotoUrl.substring(0,
+                        personPhotoUrl.length() - 2)
+                        + PROFILE_PIC_SIZE;
+ 
+                new LoadProfileImage(imgProfilePic).execute(personPhotoUrl);
+ 
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "Person information is null", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        mGoogleApiClient.connect();
+        updateUI(false);
+    }
+ 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+ 
+    /**
+     * Button on click listener
+     * */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.btn_sign_in:
+            // Signin button clicked
+            signInWithGplus();
+            break;
+        case R.id.btn_sign_out:
+            // Signout button clicked
+            signOutFromGplus();
+            break;
+        case R.id.btn_revoke_access:
+            // Revoke access button clicked
+            revokeGplusAccess();
+            break;
+        }
+    }
+ 
+    /**
+     * Sign-in into google
+     * */
+    private void signInWithGplus() {
+        if (!mGoogleApiClient.isConnecting()) {
+            mSignInClicked = true;
+            resolveSignInError();
+        }
+    }
+ 
+    /**
+     * Sign-out from google
+     * */
+    private void signOutFromGplus() {
+        if (mGoogleApiClient.isConnected()) {
+            Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+            mGoogleApiClient.disconnect();
+            mGoogleApiClient.connect();
+            updateUI(false);
+        }
+    }
+ 
+    /**
+     * Revoking access from google
+     * */
+    private void revokeGplusAccess() {
+        if (mGoogleApiClient.isConnected()) {
+            Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+            Plus.AccountApi.revokeAccessAndDisconnect(mGoogleApiClient)
+                    .setResultCallback(new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(Status arg0) {
+                            Log.e(TAG, "User access revoked!");
+                            mGoogleApiClient.connect();
+                            updateUI(false);
+                        }
+ 
+                    });
+        }
+    }
+ 
+    /**
+     * Background Async task to load user profile picture from url
+     * */
+    private class LoadProfileImage extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+ 
+        public LoadProfileImage(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+ 
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+ 
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+ 
+}
